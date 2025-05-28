75294a6f887559bdce46c4f86648a753c3daee58
Ferdynand.Monroy <fmonroys@miumg.edu.gt>
2025-05-28 10:09:41 -0600

    Bitacora implementada

--- GestionSoftware/bitacora.cpp
+++ GestionSoftware/bitacora.cpp
@@ -1,11 +1,19 @@
-#include "bitacora.h"
+//Ferdynand Monroy 9959 24 14049 Mayo 2025
+#include "Bitacora.h"
+#include <fstream>
+#include <iostream>
 
-Bitacora::Bitacora()
-{
-    //ctor
-}
-
-Bitacora::~Bitacora()
-{
-    //dtor
+void Bitacora::registrarAccion(const std::string& usuario, const std::string& accion, const std::string& appCode) {
+    std::ofstream archivo("logs/bitacora.dat", std::ios::app | std::ios::binary);
+    if (archivo.is_open()) {
+        time_t now = time(0);
+        char* dt = ctime(&now);
+        archivo << "Usuario: " << usuario
+                << " | Acción: " << accion
+                << " | Código: " << appCode
+                << " | Fecha y hora: " << dt;
+        archivo.close();
+    } else {
+        std::cerr << "Error al registrar en la bitácora.\n";
+    }
 }
--- GestionSoftware/bitacora.h
+++ GestionSoftware/bitacora.h
@@ -1,16 +1,14 @@
+//Ferdynand Monroy 9959 24 14049 Mayo 2025
 #ifndef BITACORA_H
 #define BITACORA_H
 
+#include <string>
+#include <ctime>
 
-class Bitacora
-{
-    public:
-        Bitacora();
-        virtual ~Bitacora();
-
-    protected:
-
-    private:
+class Bitacora {
+public:
+    static void registrarAccion(const std::string& usuario, const std::string& accion, const std::string& appCode);
 };
 
-#endif // BITACORA_H
+#endif
+
